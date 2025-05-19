57aea0e7e8eeaf020c763eb041ac715c5e36301c
Roeun-Chanthou <roeunchanthou1401@gmail.com>
2025-05-19 14:21:05 +0700

    first create

---
+++ lib/core/routes/routes_app.dart
@@ -0,0 +1,5 @@
+class RoutesApp {
+  RoutesApp._();
+
+  
+}
---
+++ lib/core/routes/routes_name.dart
@@ -0,0 +1,13 @@
+class RoutesName {
+  RoutesName._();
+
+  static String mainScreen = "/mainScreen";
+  static String loginScreen = "/loginScreen";
+  static String registerScreen = "/registerScreen";
+  static String homeScreen = "/homeScreen";
+  static String profileScreen = "/profileScreen";
+  static String settingsScreen = "/settingsScreen";
+  static String forgotPasswordScreen = "/forgotPasswordScreen";
+  static String resetPasswordScreen = "/resetPasswordScreen";
+  
+}
